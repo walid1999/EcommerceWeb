ae21f78bd65e69858573d23ae3b9fca076338b8e
elodieturamullien <56635114+elodieturamullien@users.noreply.github.com>
2021-01-28 23:14:27 +0100

    compte maj

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -25,4 +25,42 @@ class CompteController extends Controller
 
     return redirect('/connexion');
     }
+
+    public function modificationPseudo()
+    {
+        request()->validate([
+            'pseudo' => ['required'],
+        ]);
+        
+        auth()->user()->update([
+            'pseudo' => (request('pseudo')),
+        ]);
+
+    }
+
+    public function modificationEmail()
+    {
+        request()->validate([
+            'email' => ['required', 'email'],
+        ]);
+        
+        auth()->user()->update([
+            'email' => (request('pseudo')),
+        ]);
+    }
+
+
+    public function modificationMotDePasse()
+    {
+    request()->validate([
+        'password' => ['required', 'confirmed', 'min:8'],
+        'password_confirmation' => ['required'],
+    ]);
+
+    auth()->user()->update([
+        'mot_de_passe' => bcrypt(request('password')),
+    ]);
+
+    }
+
 }
